83c60c23488d6d1a2c8a2a4762d5698b9c681b4d
Wörner, Maximilian <maximilian.woerner@accenture.com>
2020-07-02 15:25:07 +0200

    Added function

--- run.py
+++ run.py
@@ -3,10 +3,19 @@ import os
 
 def main():
 
-    extension = '.txt'
-    rootdir = r'C:\Users\username\Desktop\test'
+    renameFilesToParentDir(r'C:\path\to\directory', '.txt')
 
-    for subdir, dirs, files in os.walk(rootdir):
+
+def renameFilesToParentDir(dir, ext='all'):
+    """
+    Renames all files inside the given directory and it's subdirectory to the name of the respective parent directory.
+
+    Arguments:
+    dir = directory to loop through
+    ext (optional) = specify if only specific extensions shall be considered (e.g. '.txt')
+    """
+
+    for subdir, dirs, files in os.walk(dir):
 
         count = 1
 
@@ -20,8 +29,16 @@ def main():
             parentFolder = os.path.basename(
                 os.path.normpath(os.path.dirname(path)))
 
-            if path.lower().endswith((extension)):
-                newFileName = parentFolder + " " + str(count) + extension
+            # File extension
+            fileExt = os.path.splitext(path)[1]
+
+            considerFile = True
+
+            if ext != 'all' and not path.lower().endswith((ext)):
+                considerFile = False
+
+            if considerFile:
+                newFileName = parentFolder + " " + str(count) + fileExt
                 newPath = os.path.join(subdir, newFileName)
 
                 os.rename(path, newPath)
@@ -31,7 +48,6 @@ def main():
                 count = count + 1
 
             else:
-
                 print("Not renamed.")
 
             print("---")
